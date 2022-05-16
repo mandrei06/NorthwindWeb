aa47f1ccac3d597b8d849f50ff1073b734690e01
Muhammad Teladia <P2577087@my365.dmu.ac.uk>
2022-05-16 17:24:02 +0100

    Solved the html issue (thanks andy)

--- src/main/java/com/sparta/northwindweb/controller/ProductsController.java
+++ src/main/java/com/sparta/northwindweb/controller/ProductsController.java
@@ -1,4 +1,23 @@
 package com.sparta.northwindweb.controller;
 
+import com.sparta.northwindweb.entities.Product;
+import com.sparta.northwindweb.repositories.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@Controller
 public class ProductsController {
+    @Autowired
+    public ProductRepository productRepo;
+
+    @GetMapping("/product/{id}")
+    public String getSupplierById(@PathVariable int id, Model model) {
+        Product productObj = productRepo.getById(id);
+        System.out.println(productObj);
+        model.addAttribute("productAttr", productObj);
+        return "productView";
+    }
 }
