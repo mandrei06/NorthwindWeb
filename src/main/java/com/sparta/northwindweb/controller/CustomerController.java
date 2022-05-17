6fa577cd253ca527f9d1f2d8933ab2c8cfc02703
DESKTOP-P1PMFAH\Andy <marincasandrei06@gmail.com>
2022-05-17 14:50:17 +0100

    CRUD-done

--- src/main/java/com/sparta/northwindweb/controller/CustomerController.java
+++ src/main/java/com/sparta/northwindweb/controller/CustomerController.java
@@ -2,9 +2,8 @@ package com.sparta.northwindweb.controller;
 
 import com.sparta.northwindweb.entities.Customer;
 import com.sparta.northwindweb.repositories.CustomerRepository;
-import org.hibernate.annotations.Cascade;
-import org.hibernate.annotations.CascadeType;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -33,10 +32,34 @@ public class CustomerController {
         return "customer";
     }
 
-    @RequestMapping(value="/customer/delete/{id}", method = RequestMethod.DELETE)
-    @ResponseBody
-    public void delete(@PathVariable String id) {
-        customerRepository.delete(customerRepository.getById(id));
+
+
+    @GetMapping("/customer/delete/{id}")
+    public String deleteCustomer(@PathVariable String id, Model model){
+        try{
+        customerRepository.deleteById(id);}
+        catch (EmptyResultDataAccessException e){
+            id="00000";
+        }
+        model.addAttribute("customer_deleted",id);
+        return "customerDeleted";
     }
+
+    @GetMapping("/customer/edit/{id}")
+    public String editCustomer(@PathVariable String id, Model model){
+        Customer thisCustomer=customerRepository.getById(id);
+        model.addAttribute("customerToEdit",thisCustomer);
+        return "editCustomerForm";
+    }
+
+    @PostMapping("/updateCustomer")
+    public String updateCustomer(@ModelAttribute("customerToEdit") Customer theCustomer){
+        Customer oldState=customerRepository.getById(theCustomer.getId());
+        oldState.setContactName(theCustomer.getContactName());
+        oldState.setPhone(theCustomer.getPhone());
+        customerRepository.save(oldState);
+        return "editSuccess";
+    }
+
 
 }
