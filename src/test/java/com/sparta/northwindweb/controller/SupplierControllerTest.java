bfbcd8b6131967e0b09a7fa8f92f2643390f4a60
pcallum <callumrp10@gmail.com>
2022-05-20 14:08:37 +0100

    Fixed supplier pages

---
+++ src/test/java/com/sparta/northwindweb/controller/SupplierControllerTest.java
@@ -0,0 +1,36 @@
+package com.sparta.northwindweb.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SupplierControllerTest {
+
+    @Test
+    void getSupplierById() {
+    }
+
+    @Test
+    void testGetSupplierById() {
+    }
+
+    @Test
+    void deleteSupplierById() {
+    }
+
+    @Test
+    void editSupplier() {
+    }
+
+    @Test
+    void addSupplier() {
+    }
+
+    @Test
+    void testAddSupplier() {
+    }
+
+    @Test
+    void updateSupplier() {
+    }
+}
